42e4931c356a2da7db4df2f48d1e16cf1585a92f
kshitish.Itachi <123272704+KshitishKumarBarik@users.noreply.github.com>
2024-02-14 23:52:59 +0530

    Update Armstrong.java

--- Number_problem/Armstrong.java
+++ Number_problem/Armstrong.java
@@ -1,39 +1,39 @@
-package Number_problem;
 import java.util.Scanner;
+
 public class Armstrong {
-	public static void m1(int num)
-	{
-		int x =0;
-		int sum = 0;
-		int y;
-		int num1 = num;	 	//Storing the initial number value inside another variable for Comparing purpose...
-		while(num!=0)
-		{
-			x= num%10;		//to separate the digit ...
-			num= num/10;	//to separate last digit from the  initial number
-			y = (x*x*x);
-			sum = sum + y;  // Sum of the cube of each digit...
-		}
-		if (sum ==num1)
-		{
-			System.out.println("This is an Armstrong Number");
-		}
-		else
-		{
-			System.out.println("This is not an Armstrong Number");
-		}
-		
-	}
+    public static void armstrong(int num) {
+        int count = 0, power = 1;
+        int num1 = num;
+        int sum = 0;
+        int x;
+        
+        while (num != 0) 
+        {
+            x = num % 10; 
+            num = num / 10; 
+            count++; // 1 //2
 
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a Number");
-		int num = sc.nextInt();
-		m1(num);
-		sc.close();
-		
+            for (int i = 1; i <= count; i++) 
+            {
+                power *= x; 
+            }
+            sum += power;   
+        }
+        if (sum == num1) {
+            System.out.println("Armstrong Number");
 
-	}
+        } else {
+            System.out.println("Not an Armstrong Number");
 
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Number");
+        int num = sc.nextInt();
+        armstrong(num);
+
+    }
 }
