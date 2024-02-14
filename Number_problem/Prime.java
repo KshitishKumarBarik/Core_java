c0668e046694f9588db883c7607e57bbbb5bc690
kshitish.Itachi <123272704+KshitishKumarBarik@users.noreply.github.com>
2024-02-15 00:26:01 +0530

    Update Prime.java

--- Number_problem/Prime.java
+++ Number_problem/Prime.java
@@ -1,30 +1,38 @@
-package Number_problem;
+
 import java.util.Scanner;
 public class Prime 
 {
 	public static void prime(int num)
 	{
-		int count= 0;   		//used to keep count of the completely divisible number....
-		for(int i =1;i<=num;i++)
-		{
-			if(num%i==0)
-			{
-				count++;
-				 
-			}
-			
-		}
+        if(num>2 && num%2==0)
+        {
+            System.out.println(" Not a Prime Number");
+        }
+        else
+        {
+
+            int count= 0;   		//used to keep count of the completely divisible number....
+            for(int i =1;i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    count++;
+                    
+                }
+                
+            }
 		 if(count==2)
 		 {
 			 System.out.println("This is a Prime Number");
 		 }
 		 else
 		 {
-			 System.out.println("This is not a Prime Number");
-		 }
-	}
-	
-
+             System.out.println("This is not a Prime Number");
+            }
+        }
+        
+    }
+        
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
@@ -37,7 +45,3 @@ public class Prime
 	}
 
 }
-/*
- *  
- * */
- 
